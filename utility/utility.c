34889433281f98809ada6569d889fc34ae1a692b
mede-erik <mede.erik@gmail.com>
2022-01-05 15:11:01 +0100

    created the utility library and added the map function to it

---
+++ utility/utility.c
@@ -0,0 +1,19 @@
+/*
+
+*/
+/**
+ * @brief transforms a number belonging to a particular range and transposes it into a new range of numbers
+ * 
+ * @param x The number to transpose
+ * @param in_min the minimum value of the first interval
+ * @param in_max the maximum value of the first interval
+ * @param out_min the minimum value of the second interval
+ * @param out_max the maximum value of the second interval
+ * @return long 
+ */
+
+long map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
