1054f9139f873c67a027a130beb96bd4c5cdb84b
mede-erik <mede.erik@gmial.com>
2022-03-22 15:01:32 +0100

    init ntc library and add temperature conversion function

--- utility/utility.c
+++ utility/utility.c
@@ -1,6 +1,6 @@
 /*
  * Author: Medeossi Erik
-*/
+ */
 
 // Includes
 #include <stdio.h>
@@ -8,13 +8,13 @@
 
 /**
  * @brief transforms a number belonging to a particular range and transposes it into a new range of numbers
- * 
+ *
  * @param x The number to transpose
  * @param in_min the minimum value of the first interval
  * @param in_max the maximum value of the first interval
  * @param out_min the minimum value of the second interval
  * @param out_max the maximum value of the second interval
- * @return long 
+ * @return long
  */
 
 long map(long x, long in_min, long in_max, long out_min, long out_max)
@@ -30,4 +30,14 @@ void swap(int *p_a, int *p_b)
 	t = *p_a;
 	*p_a = *p_b;
 	*p_b = t;
+}
+
+float kelvin_to_celsius(float kelvin)
+{
+	return kelvin - 273, 15;
+}
+
+float kelvin_to_Fahrenheit(float kelvin)
+{
+	return (kelvin_to_celsius(kelvin)) * (9 / 5) + 32;
 }
