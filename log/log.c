18438d4a2e464eeaeee4423c7b44f05fca76aa49
mede-erik <mede.erik@gmail.com>
2022-12-06 12:14:47 +0100

    update lod lib

--- log/log.c
+++ log/log.c
@@ -3,3 +3,23 @@
  * @date 06/12/2022
  *
  */
+
+#include "log.h"
+
+LOG_HandleTypeDef log_init(UART_HandleTypeDef *uartHandle, int mode)
+{
+    LOG_HandleTypeDef data;
+
+    data.uartHandle = uartHandle;
+    data.mode = mode;
+
+    return data;
+}
+
+void LOG_print_info(LOG_HandleTypeDef logHandle, char msg[25])
+{
+    if (logHandle.mode <= 3)
+    {
+        // TODO add serial print
+    }
+}
--- log/log.h
+++ log/log.h
@@ -4,7 +4,28 @@
  *
  */
 
+#include "main.h"
+#include <string.h>
+
+// labels
+
 #define ERORR_LABEL "[ERORR]:"
 #define DEBUG_LABEL "[DEBUG]:"
 #define WARN_LABEL "[WARN]:"
 #define INFO_LABEL "[INFO]:"
+#define FAULT_LABEL "[FAULT]:"
+#define MSG_LABEL "[MSG]:"
+
+// modes
+
+#define FAULT_MODE 0
+#define ERORR_MODE 1
+#define WARN_MODE 2
+#define INFO_MODE 3
+#define DEBUG_MODE 4
+
+typedef struct
+{
+    UART_HandleTypeDef *uartHandle;
+    int mode;
+} LOG_HandleTypeDef;
