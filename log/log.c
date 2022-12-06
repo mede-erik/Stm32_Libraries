7bf770d00587ba550a3587bb8baa6f0c5818028d
mede-erik <mede.erik@gmail.com>
2022-12-06 11:54:50 +0100

    add new lib

---
+++ log/log.c
@@ -0,0 +1,5 @@
+/**
+ * @author Medeossi Erik
+ * @date 06/12/2022
+ *
+ */
---
+++ log/log.h
@@ -0,0 +1,10 @@
+/**
+ * @author Medeossi Erik
+ * @date 06/12/2022
+ *
+ */
+
+#define ERORR_LABEL "[ERORR]:"
+#define DEBUG_LABEL "[DEBUG]:"
+#define WARN_LABEL "[WARN]:"
+#define INFO_LABEL "[INFO]:"
