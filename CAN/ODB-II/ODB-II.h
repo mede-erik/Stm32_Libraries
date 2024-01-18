0a48e678ddd2fdff60e2e0237e0cee7538097345
mede-erik <mede.erik@gmail.com>
2024-01-18 16:54:46 +0100

    init ODB-II lib

---
+++ CAN/ODB-II/ODB-II.h
@@ -0,0 +1,44 @@
+/**
+ * @author Medeossi Erik
+ * @date 18/01/2024
+ * @link https://github.com/mede-erik/Stm32_Libraries
+ */
+
+#ifdef STM32F0
+#include "stm32f0xx_hal.h" /* Import HAL library */
+#elif defined(STM32F1)
+#include "stm32f1xx_hal.h" /* Import HAL library */
+#elif defined(STM32F2)
+#include "stm32f2xx_hal.h" /* Import HAL library */
+#elif defined(STM32F3)
+#include "stm32f3xx_hal.h" /* Import HAL library */
+#elif defined(STM32F4)
+#include "stm32f4xx_hal.h" /* Import HAL library */
+#elif defined(STM32F7)
+#include "stm32f7xx_hal.h" /* Import HAL library */
+#elif defined(STM32G0)
+#include "stm32g0xx_hal.h" /* Import HAL library */
+#elif defined(STM32G4)
+#include "stm32g4xx_hal.h" /* Import HAL library */
+#elif defined(STM32H7)
+#include "stm32h7xx_hal.h" /* Import HAL library */
+#elif defined(STM32L0)
+#include "stm32l0xx_hal.h" /* Import HAL library */
+#elif defined(STM32L1)
+#include "stm32l1xx_hal.h" /* Import HAL library */
+#elif defined(STM32L5)
+#include "stm32l5xx_hal.h" /* Import HAL library */
+#elif defined(STM32L4)
+#include "stm32l4xx_hal.h" /* Import HAL library */
+#elif defined(STM32H7)
+#include "stm32h7xx_hal.h" /* Import HAL library */
+#else
+#endif
+
+#define OBD_PID_SPEED 0x0D
+#define OBD_PID_RPM 0x0C
+#define OBD_PID_COOLANT_TEMP 0x05
+#define OBD_PID_ERROR_CODES 0x03
+#define OBD_PID_INTAKE_PRESSURE 0x0B
+#define OBD_PID_FUEL_PRESSURE 0x0A
+#define OBD_PID_AIR_FUEL_RATIO 0x44
